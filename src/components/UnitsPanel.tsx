311ea372c6056e490f3da095899b23f2b46f499e
Sebastián Gurin <sebastigurin@gmail.com>
2018-04-29 04:47:10 -0300

    resrouces player unit panel prettier

--- src/components/UnitsPanel.tsx
+++ src/components/UnitsPanel.tsx
@@ -14,23 +14,34 @@ export class UnitsPanel extends BaseComponent<{}> {
   }
   public render() {
     // const showResources = State.get().game.showAIResourcesPanel &&
+    const playersForResources = this.state.players.filter(p => State.get().game.showAIResourcesPanel || !p.isAI)
+    const playersForUnits = this.state.players.filter(p => State.get().game.showAIUnitsPanel || !p.isAI)
     return (
-      <div className="UnitsPanel row">
-      {this.state.players.map(player =>
-        <div key={'player_div_' + player.id} className="col-6">
-          {State.get().game.showAIResourcesPanel || !player.isAI ? <PlayerResources player={player}/> : ''}
-          {State.get().game.showAIUnitsPanel || !player.isAI  ? <ul key={'player_ul_' + player.id}>{this.state.uiState.playerControls.filter(c => c.playerId === player.id).map(pc =>
-            pc.addUnitButtons.filter(ub => ub.isPrimary).map((button, i) =>
-            <li key={'player_' + player.id + '_button_' + i} className="row">
-              <UnitTypeButton button={button} player={player}/>
-            </li>,
-            ),
+      <div className="PlayerUnitsAndResourcesPanel container-fluid">
+        <div className="PlayerResourcesPanel row">
+          {playersForResources.map(player =>
+          <div key={'player_resource_div_' + player.id} className={playersForResources.length === 1 ? 'PlayerResourcesRow cols-12' : 'PlayerResourcesRow cols-6'}>
+            <PlayerResources player={player}/>
+          </div>,
           )}
-          </ul> : ''}
-        </div>,
-      )
-      }
+        </div>
+        <div className="PlayerUnitsPanel row">
+          {playersForUnits.map(player =>
+          <div key={'player_unit_div_' + player.id} className={playersForUnits.length === 1 ? 'cols-12' : 'cols-6'}>
+            <ul key={'player_ul_' + player.id}>
+            {this.state.uiState.playerControls.filter(c => c.playerId === player.id).map(pc =>
+              pc.addUnitButtons.filter(ub => ub.isPrimary).map((button, i) =>
+              <li key={'player_' + player.id + '_button_' + i} className="row">
+                <UnitTypeButton button={button} player={player}/>
+              </li>,
+              ),
+            )}
+            </ul>
+          </div>,
+          )}
+        </div>
       </div>
     )
   }
 }
+    
